2bdd3603848348d84354660af3b97e987c3b08dc
Kyle <kj.mckinley@gmail.com>
2021-06-22 21:46:21 -0500

    added jquerry code in renderLicenseSection()

--- assests/utils/generateMarkdown.js
+++ assests/utils/generateMarkdown.js
@@ -32,11 +32,15 @@ function renderLicenseLink(license) {
       return "";
   }
 
-// TODO: Create a function that returns the license section of README
+// A function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `Project is Licensed Under [${license}](${renderLicenseLink
+    (license)})`;
+}
+}
 
-// TODO: Create a function to generate markdown for README
+// A function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
