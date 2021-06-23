0b2bf59fbe41de1e35834ebd84458c4375a3ec6a
Kyle <kj.mckinley@gmail.com>
2021-06-22 22:15:24 -0500

    updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./assests/utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // An array of questions for user input
 const questions = [
